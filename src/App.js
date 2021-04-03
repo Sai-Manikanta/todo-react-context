5763d2b43fa4fe33f54c529301c8fd0766ddefbd
mani333007 <mani333007@gmail.com>
2021-04-03 10:29:53 +0530

    Added themeContext and wrapping up in App.js

--- src/App.js
+++ src/App.js
@@ -2,11 +2,16 @@ import React from 'react';
 import BookList from './components/BookList';
 import Navbar from './components/NavBar';
 
+// contexts
+import ThemeContextProvider from './contexts/ThemeContext';
+
 function App() {
   return (
     <div className="App">
-      <Navbar />
-      <BookList />
+      <ThemeContextProvider>
+        <Navbar />
+        <BookList />
+      </ThemeContextProvider>
     </div>
   );
 }
